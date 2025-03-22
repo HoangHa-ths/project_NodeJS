2dd837a7e4aea5ffafec692bdb6f36a1a1ef7fd4
HTTH <ham2524006@gstudent.ctu.edu.vn>
2025-03-22 10:00:03 +0700

    them bang

--- app.js
+++ app.js
@@ -4,7 +4,50 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end(`<html>
+      <head>
+        <meta charset="UTF-8">
+        <title>Bảng Thông Tin Sinh Viên</title>
+        <style>
+          table {
+            width: 100%;
+            border-collapse: collapse;
+          }
+          table, th, td {
+            border: 1px solid black;
+          }
+          th, td {
+            padding: 8px;
+            text-align: left;
+          }
+          th {
+            background-color: #f2f2f2;
+          }
+        </style>
+    </head>
+    <table>
+      <thead>
+          <tr>
+              <th>MSHV</th>
+              <th>Họ tên</th>
+              <th>Ngày sinh</th>
+              <th>Quê quán</th>
+              <th>Email</th>
+              <th>SĐT</th>
+          </tr>
+      </thead>
+      <tbody>
+          <td>
+              <td>M2524006</td>
+              <td>Hoàng Thị Thanh Hà</td>
+              <td>25/11/2001</td>
+              <td>Kiên Giang</td>
+              <td>0836251101</td>
+          </td>
+      </tbody>
+    </table>
+  </html>
+  `);
 });
 
 server.listen(port,() => {
